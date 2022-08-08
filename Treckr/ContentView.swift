eacd1808ef0e53e109350c75f7357e3f96236db4
vibhuti-shah-mutualmobile <vibhuti.shah@mutualmobile.com>
2022-08-08 18:04:57 +0530

    Initial Commit

---
+++ Treckr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Treckr
+//
+//  Created by Hasmukh Shah on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Treckr/TreckrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreckrApp.swift
+//  Treckr
+//
+//  Created by Hasmukh Shah on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TreckrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
